4e406d173805095f7998ba91100ff163fcc40392
LA\Gerardo Garita <ggaritajuarez@gmail.com>
2018-09-17 11:39:59 -0600

    GGarita, variable sin uso declarada

--- example/js/expandableOptions-1.0.js
+++ example/js/expandableOptions-1.0.js
@@ -8,7 +8,6 @@
 ///////////////////////////////////////////////
 
 ; (function ($) {
-    var classExpandable;
     jQuery.fn.ggExpandable = function (options) {
         try {
             var path = "./";
